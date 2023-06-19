c859edab964959571024cddecc910f30dcb32118
Ian Mande <36795991+ianmande@users.noreply.github.com>
2023-06-19 18:04:04 -0400

    Build/configure setup (#2)
    * config intial vue
    
    * added vue-devtools and remove basic url in tsconfig
    
    * install husky
    
    * try added console.log
    
    * addd lint stage
    
    * try commit with error in file
    
    * build: install commitlint and configure commit-msg
    
    * build: added commitizen
    
    * chore: added script to commit msg
    
    ---------
    
    Co-authored-by: Ian Mande <isaac@mftech.io>

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import devtools from '@vue/devtools'
+
+import App from './App.vue'
+import router from './router/index'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
+
+if (process.env.SETUP_ENV === 'development') {
+  devtools.connect()
+}
